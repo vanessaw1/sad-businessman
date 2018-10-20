79916e29b91d60e6d5c4ca752860487d90cc6066
vanessaw1 <wang.vanessa1@gmail.com>
2018-10-20 00:28:44 -0400

    Starter code

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <div>
+                        <Link to="/main">Main</Link>
+                        <Link to="/map">Map</Link>
+                    </div>
+                    <div>
+                        <Route path="/main" component={Main} />
+                        <Route path="/map" component={Map} />
+                    </div>
+                </div>
+            </Router>
+        );
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Main extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <div>
+                        This is the main page
+                    </div>
+                </div>
+            </Router>
+        );
+    }
+}
+
+class Map extends React.Component {
+    render() {
+        return (
+            <div>
+                This is the map
+            </div>
+        );
+    }
+}
+
+// ========================================
+// export default App;
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
