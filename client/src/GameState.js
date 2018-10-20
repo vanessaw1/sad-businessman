b21019d7d9ede41f02f237a03c3d94de14186ec6
vanessaw1 <wang.vanessa1@gmail.com>
2018-10-20 00:45:00 -0400

    yilin-fix bugs

--- client/src/GameState.js
+++ client/src/GameState.js
@@ -4,9 +4,9 @@ class PlayerData extends React.Component {
     constructor(props) {
         super(props);
         this.state = {skills: {"bribe":1, "pr":1, "ads":1, "product":1, "profit":1},
-                      skillsChange : {"bribe":0, "pr":0, "ads":0, "product":0, "profit":0},
-                      scores: {"efficiency":1, "reputation":1, "money":1 }
-                     };
+                    skillsChange : {"bribe":0, "pr":0, "ads":0, "product":0, "profit":0},
+                    scores: {"efficiency":1, "reputation":1, "money":1 }
+                    };
     }
 
     updateSkills() {
@@ -14,14 +14,15 @@ class PlayerData extends React.Component {
     }
 
     updateScores() {
-        newScores = {"efficiency":this.state.scores.efficiency*2, 
+        var newScores = {"efficiency":this.state.scores.efficiency*2, 
                      "reputation":this.state.scores.reputation*2, 
                      "money":this.state.scores.money*2 }
         return newScores;
     }
 
     update() {
-        this.setState = {skills: updateSkills(), scores: updateScores()};
+        this.setState = {skills: this.updateSkills(), scores: this.updateScores()};
+        return this;
     }
 
     getScores() {
@@ -49,20 +50,23 @@ export default class GameState extends React.Component {
         this.timerID = setInterval(
             () => this.tick(),
             1000
-          );
+        );
     }
-  
+
     componentWillUnmount() {
         clearInterval(this.timerID);
     }
 
     tick() {
+        var aTick = this.state.PlayerData.update();
         this.setState({
-            PlayerData: this.state.PlayerData.update()
+            PlayerData: aTick
         });
-      }
+        alert(aTick.getScores())
+    }
 
     render() {
+
         return (
             <div> 
                 <h1>The businessman has money {this.state.PlayerData.getScores().money}.</h1>
