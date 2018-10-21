a1e992debfa3559cf5073a3b13bb84f0a90b822c
“jl3663” <jl3663@cornell.edu>
2018-10-21 00:22:05 -0400

    underline

--- client/src/Components/Sidebar.js
+++ client/src/Components/Sidebar.js
@@ -6,27 +6,27 @@ class Sidebar extends React.Component {
     render() {
         return (
             <div className="Sidebar">
-                <Link to="/overview">
+                <Link to="/overview" style={{ textDecoration: 'none' }}>
                     <button className="Overview nav">
                         Overview
                     </button>
                 </Link>
-                <Link to="/map">
+                <Link to="/map" style={{ textDecoration: 'none' }}>
                     <button className="Map nav">
                         Map
                     </button>
                 </Link>
-                <Link to="/skillpoints">
+                <Link to="/skillpoints" style={{ textDecoration: 'none' }}>
                     <button className="Skillpoints nav">
                         Skillpoints
                     </button>
                 </Link>
-                <Link to="/factories">
+                <Link to="/factories" style={{ textDecoration: 'none' }}>
                     <button className="Factories nav">
                         Factories
                     </button>
                 </Link>
-                <Link to="/inbox">
+                <Link to="/inbox" style={{ textDecoration: 'none' }}>
                     <button className="Inbox nav">
                         Inbox
                     </button>
